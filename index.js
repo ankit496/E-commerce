6e2d3fd0db208627d45a03339b0ff84c93b4afdc
ankit496 <akankit496@gmail.com>
2024-02-01 13:13:52 +0530

    Added some basic APIs

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express=require('express')
 const server=express()
 const connectMongoose=require('./dbConnect')
+const { createProduct } = require('./controller/Product')
+const productRouter=require('./routes/Products')
+const categoriesRouter=require('./routes/Categories')
+const brandsRouter=require('./routes/Brands')
+const cors=require('cors')
+
+//middlewares
+server.use(cors({
+    exposedHeaders:['X-Total-Count']
+}))
+server.use(express.json())
+server.use('/products',productRouter.router)
+server.use('/categories',categoriesRouter.router)
+server.use('/brands',brandsRouter.router)
+
+
 server.get('/',(req,res)=>{
     res.json({status:'success'})
 })
+
+
+
 connectMongoose()
-server.listen(8080,()=>{
-    console.log('server started on port 8080')
+server.listen(8000,()=>{
+    console.log('server started on port 8000')
 })
